29876edacac5067471f6a5ab6b2893466e862c18
Nhat-Hoang <nguyenph001@e.ntu.edu.sg>
2024-10-26 16:30:29 +0800

    hotfix setup scripts

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ setup(
     description='Basic logic for demand-capacity balancing in ATFM',
     author='Nhat-Hoang P. Nguyen',
     packages=find_packages(),
-    install_requires=install_requires,  # Use the list of dependencies from requirements.txt
+    # install_requires=install_requires,  # Use the list of dependencies from requirements.txt
+    install_requires=[
+        "intervaltree=3.1.0",
+        "numpy=1.25.2",
+        "pandas=2.0.3",
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
